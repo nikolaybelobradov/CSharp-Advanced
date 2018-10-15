e10eb8557aa9c9631d822bc4501867d781d91801
Nikolay Belobradov <nikolaybelobradov@gmail.com>
2018-10-15 22:06:19 +0300

    Add files via upload

---
+++ Exams/Exam-2018-10-14/CupsAndBottles.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CupsAndBottles
+{
+    class CupsAndBottles
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapacity = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] filledBottles = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> cupsQueue = new Queue<int>();
+            Stack<int> bottlesStack = new Stack<int>();
+
+            for (int i = 0; i < cupsCapacity.Length; i++)
+            {
+                cupsQueue.Enqueue(cupsCapacity[i]);
+            }
+
+            for (int i = 0; i < filledBottles.Length; i++)
+            {
+                bottlesStack.Push(filledBottles[i]);
+            }
+
+            int wastedWater = 0;
+
+            while (true) { 
+
+                int currentCup = cupsQueue.Dequeue();
+
+                while (currentCup > 0)
+                {
+                    int currentBottle = bottlesStack.Pop();
+
+                    currentCup -= currentBottle;
+
+                    if(currentCup < 0)
+                    {
+                        wastedWater += Math.Abs(currentCup);
+                    }
+                }
+
+                if(bottlesStack.Count == 0 || cupsQueue.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if(bottlesStack.Count == 0)
+            {
+                int[] temp = cupsQueue.ToArray();
+                Console.Write("Cups: ");
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    if (i == temp.Length - 1)
+                    {
+                        Console.Write($"{temp[i]}");
+                    }
+                    else
+                    {
+                        Console.Write($"{temp[i]} ");
+                    }
+                }
+                
+            }else if(cupsQueue.Count == 0)
+            {
+
+                int[] temp = bottlesStack.ToArray();
+                temp.Reverse();
+
+                Console.Write("Bottles: ");
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    if (i == temp.Length - 1)
+                    {
+                        Console.Write($"{temp[i]}");
+                    }
+                    else
+                    {
+                        Console.Write($"{temp[i]} ");
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
---
+++ Exams/Exam-2018-10-14/Tagram.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tagram
+{
+    class Tagram
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(new[] { ' ', '-', '>'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var database = new Dictionary<string, Dictionary<string, int>>();
+
+           
+
+            while(input[0] != "end")
+            {
+
+                if (input[0] == "ban")
+                {
+                    string user = input[1];
+
+                    if (database.ContainsKey(user))
+                    {
+                        database.Remove(user);
+                    }
+
+
+                }
+                else
+                {
+
+                    string username = input[0];
+                    string tag = input[1];
+                    int likes = int.Parse(input[2]);
+
+                    if (!database.ContainsKey(username))
+                    {
+                        database.Add(username, new Dictionary<string, int>());
+                    }
+
+                    if (!database[username].ContainsKey(tag))
+                    {
+                        database[username].Add(tag, 0);
+                    }
+
+                    database[username][tag] += likes;
+                }
+                    input = Console.ReadLine()
+                    .Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                
+            }
+
+            var sumOfLikes = new Dictionary<string, Dictionary<int, int>>();
+
+            foreach (var user in database)
+            {
+                if (!sumOfLikes.ContainsKey(user.Key))
+                {
+
+                    int sumLikes = 0;
+                    int sumTags = 0;
+                    foreach (var item in user.Value)
+                    {
+                        sumLikes += item.Value;
+                        sumTags++;
+                    }
+                    sumOfLikes.Add(user.Key, new Dictionary<int, int>());
+                    sumOfLikes[user.Key].Add(sumLikes, sumTags);
+                }
+            }
+
+            foreach (var outer in database.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x=> x.Value.Count))
+            {
+                Console.WriteLine(outer.Key);
+                foreach (var inner in outer.Value)
+                {
+                    Console.WriteLine($"- {inner.Key}: {inner.Value}");
+                }
+            }
+
+            
+        }
+    }
+}
